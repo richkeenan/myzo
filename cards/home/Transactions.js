52a3eeb114d16eaabc4f68f5cb71e2482bf8422f
Richard Keenan <rskeenan@gmail.com>
2018-09-23 16:39:37 +0100

    Removed some copy/paste error duplicated code

--- cards/home/Transactions.js
+++ cards/home/Transactions.js
@@ -1,21 +1,11 @@
 import React from "react";
 import { SectionList } from "react-native";
 import PropTypes from "prop-types";
-import isToday from "date-fns/is_today";
-import isYesterday from "date-fns/is_yesterday";
-import format from "date-fns/format";
 import Transaction from "./Transaction";
 import SectionHeader from "./SectionHeader";
 import { Link } from "react-router-native";
 
 class Transactions extends React.Component {
-  transactionDate = date => {
-    if (isToday(date)) return "Today";
-    if (isYesterday(date)) return "Yesterday";
-
-    return format(date, "dddd DD MMMM");
-  };
-
   onViewableItemsChanged = items => {
     const selectedDate = items.viewableItems[0].item.rawDate;
     this.props.onSelectedDateChanged(selectedDate);
